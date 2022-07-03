8815ac03489e77e22ebdf44bd5dc5ed56f538f9a
Ghanem <ghanemg@uwindsor.ca>
2022-07-02 20:40:02 -0400

    can now access bitly account

--- script.py
+++ script.py
@@ -1,12 +1,21 @@
 import webbrowser
 import argparse
+import requests
+import logging
 
 parser = argparse.ArgumentParser(description='Enter your credentials')
 
 parser.add_argument('Username', type=str, help='Username for bitly API')
 parser.add_argument('Password', type=str, help='Password for bitly API')
-parser.add_argument('token', type=str, help='Access token')
 
 args = parser.parse_args()
 
-auth_res = request.post(args.token, auth(args.Username,Password))
+auth_res = requests.post('https://api-ssl.bitly.com/oauth/access_token', auth=(args.Username,args.Password))
+
+if auth_res.status_code ==200:
+	access_token = auth_res.content.decode()
+	print("Access token worked", access_token)
+else:
+	logging.error("Did not get Access token, exiting...")
+	exit()
+
